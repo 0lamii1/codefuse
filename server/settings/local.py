c59a5d825850aa2332924aac93b4d7f3917bd409
emekadefirst <victorchibuogwu33@gmail.com>
2025-09-15 04:04:20 +0100

    origin

--- server/settings/local.py
+++ server/settings/local.py
@@ -1,11 +1,12 @@
 from .base import *
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['codefuse.sevalla.app']
 
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000'
+    'https://codefuse.sevalla.app',
 ]
 
 
